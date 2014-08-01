5fd6e770bc838813cfb0fb0c9133490de0118f41
Matt Geller <matt@mmct.com>
2014-08-01 16:31:34 -0500

    Adding real scripts now.

---
+++ job_decreaser.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+"""
+
+ MetaCode Job Decreaser, version 1.0
+ Copyright (c) 2014, MetaCode, Inc. All Rights Reserved.
+ Call this puppy via cron when you want to decrease simultaneous jobs.
+
+"""
+
+__author__ = 'MetaCode, Inc.'
